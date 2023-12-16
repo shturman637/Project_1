866a45af36ddcbda85e2e76e797eec02ac9a0dfe
Irina Artemyeva <irika.artemyeva@gmail.com>
2023-12-16 23:55:23 +0500

    Irina Artemeva opus_mit_sla_en

--- opus_mt_sla_en.py
+++ opus_mt_sla_en.py
@@ -2,12 +2,13 @@ from transformers import pipeline
 import streamlit as st
 
 st.title('Переводчик со славянских языков на английский')
-Text_source = st.text_input('Введите текст: ')
+Text_source = st.text_input('Введите текст: ', (
+        label='Ваш текст'))
 
 @st.cache(allow_output_mutation=True)
 def translate_model():
     classifier = pipeline("translation", model="Helsinki-NLP/opus-mt-sla-en")
     classifier(Text_source)
-    return translate_model
+    return classifier
 
 st.write('Перевод: ', translate_model)
