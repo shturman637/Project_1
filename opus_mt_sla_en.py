55ed94c87b08989e69d148899b99e0e9eb056623
Irina Artemyeva <irika.artemyeva@gmail.com>
2023-12-17 03:35:50 +0500

    opus_mit_sla_en design edited

--- opus_mt_sla_en.py
+++ opus_mt_sla_en.py
@@ -4,12 +4,16 @@ import streamlit as st
 st.title('Переводчик со славянских языков на английский')
 Text_source = st.text_input('Введите текст: ')
 
-@st.cache(allow_output_mutation=True)
-def translate_model(Text_source):
-    classifier = pipeline("translation", model="Helsinki-NLP/opus-mt-sla-en")
-    translation_result = classifier(Text_source, max_length=50)
-    return translation_result[0]['translation_text']
-
 if Text_source:
+    with st.spinner('Выполняется перевод...'):
+        @st.cache(allow_output_mutation=True)
+        def translate_model(Text_source):
+            classifier = pipeline("translation", model="Helsinki-NLP/opus-mt-sla-en")
+            translation_result = classifier(Text_source, max_length=50)
+            return translation_result[0]['translation_text']
+
     translation = translate_model(Text_source)
+ 
+     # Остановка спиннера и вывод результата
+    st.success('Перевод выполнен успешно!')
     st.write('Перевод:', translation)
