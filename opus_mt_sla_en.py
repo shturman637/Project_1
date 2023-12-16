6ae4f61b0898a907f87eb30ba26ef90026334ae1
Irina Artemyeva <irika.artemyeva@gmail.com>
2023-12-16 23:26:32 +0500

    Irina Artemeva opus-mt-sla-en bugfix

--- opus_mt_sla_en.py
+++ opus_mt_sla_en.py
@@ -4,6 +4,7 @@ import streamlit as st
 st.title('Переводчик со славянских языков на английский')
 Text_source = st.text_input('Введите текст: ')
 
+@st.cache(allow_output_mutation=True)
 def translate_model():
     classifier = pipeline("translation", model="Helsinki-NLP/opus-mt-sla-en")
     classifier(Text_source)
