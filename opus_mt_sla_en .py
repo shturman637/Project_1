b5331da679635161aa6be9dbe48d743678127a80
Irina Artemyeva <irika.artemyeva@gmail.com>
2023-12-16 22:28:09 +0500

    Irina Artemeva HW 2
    Streamlit app

---
+++ opus_mt_sla_en .py
@@ -0,0 +1,13 @@
+from transformers import pipeline
+import streamlit as st
+
+st.title('Переводчик со славянских языков на английский')
+Text_source = st.text_input('Введите текст: ')
+
+def translate_model():
+    classifier = pipeline("translation", model="Helsinki-NLP/opus-mt-sla-en")
+    classifier(Text_source)
+    return translate_model
+
+st.write('Перевод: ', translate_model)
+
