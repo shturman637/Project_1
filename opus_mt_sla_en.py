d949dd109256310cc3ee3cd07763610e19dc5214
Irina Artemyeva <irika.artemyeva@gmail.com>
2023-12-16 23:58:01 +0500

    opus_mit_sla_en

--- opus_mt_sla_en.py
+++ opus_mt_sla_en.py
@@ -2,8 +2,7 @@ from transformers import pipeline
 import streamlit as st
 
 st.title('Переводчик со славянских языков на английский')
-Text_source = st.text_input('Введите текст: ', (
-        label='Ваш текст'))
+Text_source = st.text_input('Введите текст: ')
 
 @st.cache(allow_output_mutation=True)
 def translate_model():
