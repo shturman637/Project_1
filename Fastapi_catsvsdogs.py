a98578b55109e2c338cb78e932f1c9a909937ec5
Irina Artemyeva <irika.artemyeva@gmail.com>
2023-12-22 04:51:39 +0500

    hw 3

--- Fastapi_catsvsdogs.py
+++ Fastapi_catsvsdogs.py
@@ -8,7 +8,6 @@ from pydantic import BaseModel
 class ItemRequest(BaseModel):
     img_url: str
 
-
 class ItemResponse(BaseModel):
     prediction: float
 
@@ -16,21 +15,23 @@ app = FastAPI()
 @app.post("/predict/")
 
 def predict(request: ItemRequest):
-    img_url = 'https://ichef.bbci.co.uk/news/640/cpsprodpb/42E2/production/_96522171_gettyimages-450864271.jpg'
+    model = from_pretrained_keras("carlosaguayo/cats_vs_dogs")
+    ROWS, COLS = 150, 150
+    img_url = request.img_url
 
     img = io.imread(img_url)
     img = cv2.resize(img, (ROWS, COLS), interpolation=cv2.INTER_CUBIC)
     img = img / 255.0
     img = img.reshape(1,ROWS,COLS,3)
 
-    return ItemResponse(
     prediction = model.predict(img)[0][0]
+        
     if prediction >= 0.5:
-        print('I am {:.2%} sure this is a Cat'.format(prediction))
+        result = 'I am {:.2%} sure this is a Cat'.format(prediction)
     else:
-        print('I am {:.2%} sure this is a Dog'.format(1-prediction))
-
+        result = 'I am {:.2%} sure this is a Dog'.format(1 - prediction)
     plt.imshow(img[0], 'Blues')
     plt.axis("off")
     plt.show()
-    )
+
+    return ItemResponse(prediction=prediction, result=result)
