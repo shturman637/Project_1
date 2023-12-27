66463fef1f28faf9d6d78c83ab8303a9493ec9c2
shturman <shturman2605@mail.ru>
2023-12-28 00:52:29 +0300

    Added detection-fast.py

---
+++ detection-fast.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from transformers import pipeline
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    text: str
+
+app = FastAPI()
+classifier  = pipeline("sentiment-analysis", "cointegrated/rubert-tiny2-cedr-emotion-detection")
+
+
+@app.post("/predict/")
+def predict(item: Item):
+    """модель показывает эмоциональную окраску текста"""
+    return classifier(item.text )[0]
