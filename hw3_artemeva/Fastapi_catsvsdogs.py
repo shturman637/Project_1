894828aeaa0aefed571aca23ef1ef1c894c04859
Irina Artemyeva <irika.artemyeva@gmail.com>
2023-12-27 14:23:25 +0500

    img loader edited

--- hw3_artemeva/Fastapi_catsvsdogs.py
+++ hw3_artemeva/Fastapi_catsvsdogs.py
@@ -1,38 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
+from PIL import Image
 from skimage import io
 import cv2
 import matplotlib.pyplot as plt
 from huggingface_hub import from_pretrained_keras
-from pydantic import BaseModel
 
-class ItemRequest(BaseModel):
-    img_url: str
-
-class ItemResponse(BaseModel):
+class ItemResponse():
     prediction: float
     result: str
 
 app = FastAPI()
-@app.post("/predict/")
-
-def predict(request: ItemRequest):
-    model = from_pretrained_keras("carlosaguayo/cats_vs_dogs")
-    ROWS, COLS = 150, 150
-    img_url = request.img_url
-
-    img = io.imread(img_url)
-    img = cv2.resize(img, (ROWS, COLS), interpolation=cv2.INTER_CUBIC)
-    img = img / 255.0
-    img = img.reshape(1,ROWS,COLS,3)
-
-    prediction = model.predict(img)[0][0]
+@app.post("/recognize")
+async def recognize(file: UploadFile):
+    image_data = file.file.read()
+    img = Image.open(io.BytesIO(image_data))
+    result = list()
+    
+    if img is not None:
+        model = from_pretrained_keras("carlosaguayo/cats_vs_dogs")
         
-    if prediction >= 0.5:
-        result = 'I am {:.2%} sure this is a Cat'.format(prediction)
-    else:
-        result = 'I am {:.2%} sure this is a Dog'.format(1 - prediction)
-    plt.imshow(img[0], 'Blues')
-    plt.axis("off")
-    plt.show()
+        ROWS, COLS = 150, 150
+        img = io.imread(io.BytesIO(image_data))
+        img = cv2.resize(img, (ROWS, COLS), interpolation=cv2.INTER_CUBIC)
+        img = img / 255.0
+        img = img.reshape(1, ROWS, COLS, 3)
+
+        prediction = model.predict(img)[0][0]
+        
+        if prediction >= 0.5:
+            result = 'I am {:.2%} sure this is a Cat'.format(prediction)
+        else:
+            result = 'I am {:.2%} sure this is a Dog'.format(1 - prediction)
+
+        plt.imshow(img[0], 'Blues')
+        plt.axis("off")
+        plt.show()
 
     return ItemResponse(prediction=prediction, result=result)
