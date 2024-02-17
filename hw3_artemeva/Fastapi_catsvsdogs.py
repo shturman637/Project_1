28990b8014d66d45061ca272003447f5527c38e8
Irina Artemyeva <irika.artemyeva@gmail.com>
2024-02-17 22:05:07 +0500

    Исправления стиля кода

--- hw3_artemeva/Fastapi_catsvsdogs.py
+++ hw3_artemeva/Fastapi_catsvsdogs.py
@@ -5,19 +5,23 @@ import cv2
 import matplotlib.pyplot as plt
 from huggingface_hub import from_pretrained_keras
 
+
 class ItemResponse():
     result: str
 
+
 app = FastAPI()
+
+
 @app.post("/recognize")
 async def recognize(file: UploadFile):
     image_data = file.file.read()
     img = Image.open(io.BytesIO(image_data))
     img = list()
-    
+
     if img is not None:
         model = from_pretrained_keras("carlosaguayo/cats_vs_dogs")
-        
+
         ROWS, COLS = 150, 150
         img = io.imread(io.BytesIO(image_data))
         img = cv2.resize(img, (ROWS, COLS), interpolation=cv2.INTER_CUBIC)
@@ -25,7 +29,7 @@ async def recognize(file: UploadFile):
         img = img.reshape(1, ROWS, COLS, 3)
 
         prediction = model.predict(img)[0][0]
-        
+
         if prediction >= 0.5:
             result = 'I am {:.2%} sure this is a Cat'.format(prediction)
         else:
