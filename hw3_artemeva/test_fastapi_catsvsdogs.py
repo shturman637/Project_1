28d673ef6bf58f22ae8b505b4ed724d280019bd4
shturman <shturman2605@mail.ru>
2024-03-27 00:30:01 +0300

    Added file test_fastapi_catsvsdogs.py

--- hw3_artemeva/test_fastapi_catsvsdogs.py
+++ hw3_artemeva/test_fastapi_catsvsdogs.py
@@ -7,10 +7,18 @@ from Fastapi_catsvsdogs import app
 def client():
     return TestClient(app)
 
-def test_fastapi_catsvsdogs(client):
+def test_cats(client):
 
     with open("https://koshka.top/uploads/posts/2023-10/1696329610_koshka-top-p-malie-koshki-67.jpg", "rb") as img_file:
         files = {"file": ("https://koshka.top/uploads/posts/2023-10/1696329610_koshka-top-p-malie-koshki-67.jpg", img_file, "image/jpeg")} 
         response = client.post("/recognize", files=files)
     assert response.status_code == 200
     assert "I am" in response.json()["cat"]
+def test_dogs(client):
+
+    with open("https://gas-kvas.com/grafic/uploads/posts/2023-10/1696601772_gas-kvas-com-p-kartinki-s-sobakami-22.jpg", "rb") as img_file: 
+        files = {"file": ("https://gas-kvas.com/grafic/uploads/posts/2023-10/1696601772_gas-kvas-com-p-kartinki-s-sobakami-22.jpg", img_file, "image/jpeg")} 
+        response = client.post("/recognize", files=files)
+
+    assert response.status_code == 200
+    assert "I am" in response.json()["dog"]
